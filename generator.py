6854311d23c51998f6248f67715a7796e47fd62a
hun7err <hun7err@gmail.com>
2013-04-09 11:49:20 +0200

    Now only one thing doesn't work

--- generator.py
+++ generator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python2
-from math import ceil
+from math import floor
 from math import sqrt
 from mazakodron import *
 from svg.path import *
@@ -11,13 +11,16 @@ def dist2(curve, p, t):
 def func(curve, p, t, a):
 	return dist2(curve, p, t) - a**2
 
+def distance(a, b):
+	return sqrt((a.real - b.real)**2 + (a.imag - b.imag)**2)
+
 def bezier_points(curve, a, eps):
 	p = curve.start
 	t = 0
 	left = 0
 	right = 1
 	points = []
-	
+	#points.append(curve.start)
 	while t < 1:
 		left = t
 		right = 1
@@ -32,6 +35,92 @@ def bezier_points(curve, a, eps):
 		t = right
 		p = curve.point(t)
 		points.append(p)
+	lastt = curve.point(1)
+	d = distance(p, lastt)
+	if lastt.imag - p.imag == 0:
+		AB = a
+	else:
+		AB = float(a*abs(lastt.real-p.real))/float(d)
+	if lastt.real - p.real == 0:
+		BC = a
+	else:
+		BC = float(AB*abs(lastt.imag - p.imag)) / float(abs(p.real - lastt.real))
+	lastx = 0
+	lasty = 0
+
+	if(p.real > lastt.real):
+		sgn = 1
+	else:
+		sgn = -1
+	lastx = p.real + sgn * AB
+	if(p.imag > lastt.imag):
+		sgn = 1
+	else:
+		sgn = -1
+	lasty = p.imag + sgn * BC
+	last = complex(lastx,lasty)
+	points.append(last)
+
+	return points
+
+def line_points(line, a):
+	d = distance(line.start, line.end)
+	x_diff = 0
+	y_diff = 0
+
+	# naprawic odleglosci tutaj bo wychodza jakies popieprzone, np. 18
+
+	if d < a:
+		if line.end.imag - line.start.imag == 0:
+			x_diff = a
+		else:
+			x_diff = float(a*abs(line.end.real - line.start.real))/float(d)
+		if line.start.real - line.end.real == 0:
+			y_diff = a
+		else:
+			y_diff = float(x_diff * abs(line.end.imag - line.start.imag)) / float(abs(line.start.real - line.end.real))
+		d = a
+	else:
+		x_diff = abs(line.start.real - line.end.real)
+		y_diff = abs(line.start.imag - line.end.imag)
+		# oblicz z talesa punkt tak, aby przychodzil przez line.start i line.end ale lezal w odleglosci a
+		# w innym wypadku licz normalnie x_diff, y_diff
+		#
+		# rozwazyc przypadek, gdy d < a ale punkty leza w linii prostej
+	n = int(floor(float(d)/float(a)))
+	#x_diff = abs(line.start.real - line.end.real)
+	#y_diff = abs(line.start.imag - line.end.imag)
+	
+	#if n != 0:
+	x_off = float(x_diff)/float(n)
+	y_off = float(y_diff)/float(n)
+	#else:
+	#	x_off = y_off = 0
+
+	# czy to aby na pewno jest dobrze?
+	# czy zapewnione jest przejscie o krok a?
+	# lol, nope
+
+	x = line.start.real
+	y = line.start.imag
+
+	points = []
+	sgn_x = 0
+	sgn_y = 0
+	if (line.start.real < line.end.real):
+		sgn_x = 1
+	else:
+		sgn_x = -1
+	if (line.start.imag < line.end.imag):
+		sgn_y = 1
+	else:
+		sgn_y = -1
+	
+	for i in range(1,n+2):
+		points.append(complex(x,y))
+		x += sgn_x * x_off
+		y += sgn_y * y_off
+	#print "a = %(a)d, dist = %(d)f" % {"a" : a, "d":distance(points[0], points[-1])}
 	return points
 
 if __name__ == "__main__":
@@ -55,38 +144,107 @@ if __name__ == "__main__":
 	aby dystans dzielil sie na rowne odcinki dlugosci a gdzie a jest
 	dokladnoscia
 
-	to-do wazne:
-	zastanowic sie nad efektem bledow zaokraglen numerycznych -
-	- nad ich wplywem na ostateczne rozwiazanie
+	uklad wspolrzednych:
+	+------> x
+	|
+	|
+	|
+	v y
+
 	'''
+
+	# <TEST>
+	'''
+	p0 = complex(112.3125, 913.29968)
+	p1 = complex(512.3125, 912.17468)
+
+	tab = line_points(Line(p0,p1), 25)
+	for el in tab:
+		print "%(x)f %(y)f" % {"x":el.real, "y":el.imag}
+	'''
+	# </TEST>
+	
 	filename = ''
 	try:
 		filename = argv[1]
 	except IndexError:
 		raise AssertionError('Brak nazwy pliku')
+	a = 10
+	eps = 0.001
+
 	mazak = Mazakodron()
 	mazak.load(filename)
 	#print mazak.getDocDims()
 	mazak.loadPaths()
+	print "START"
+	
+	P = complex(0,0)
+
 	for path in mazak.getPaths():
 		for el in path:
+			#print "P = %f %f" % (P.real, P.imag)
+			#print "el = %f %f" % (el.start.real, el.start.imag)
+			if P == complex(0,0):
+				line = Line(P, complex(el.start.real+mazak.xoffset,el.start.imag+mazak.yoffset))
+			else:
+				line = Line(P, el.start)
+			if line.start != line.end:
+				#linepoints = line_points(line, a)
+				linepoints = bezier_points(line, a, eps)
+				for point in linepoints:
+					#if point != line.start:
+					print "%(x)f %(y)f" % {"x": point.real, "y":point.imag}
+				last = complex(0,0)
+				if P == complex(0,0):
+					last = complex(linepoints[-1].real-mazak.xoffset,linepoints[-1].imag-mazak.yoffset)
+				else:
+					last = linepoints[-1]
+				mazak.xoffset += last.real - el.start.real
+				mazak.yoffset += last.imag - el.start.imag
+				
+				# tutaj dodac offsety na x i y (mazak.xoffset, mazak.yoffset)
+				# i znak w zaleznosci od tego czy pierwotne > nowe czy nie
+
 			print "RYSUJ"
 			ptype = str(type(el))
 			ptype = ptype[ptype.rfind('.')+1:ptype.rfind("'")]
-
+			points = []
+			
 			if ptype == "CubicBezier":
 				#print "Cubic Bezier"
-				points = bezier_points(el, 2, 0.001)
+				points = bezier_points(el, a, eps)
 				for p in points:
-					print "%(x)f %(y)f" % {"x":p.real, "y":p.imag}
+					print "%(x)f %(y)f" % {"x":p.real+mazak.xoffset, "y":p.imag+mazak.yoffset}
+				#P = points[-1]
 			elif ptype == "QuadraticBezier":
-				print "Quadratic Bezier"
-			elif ptype == "Arc":
-				print "Arc"
+				#print "Quadratic Bezier"
+				points = bezier_points(el, a, eps)
+				for p in points:
+					print "%(x)f %(y)f" % {"x":p.real+mazak.xoffset, "y":p.imag+mazak.yoffset}
+				#P = points[-1]
+				#print "Quadratic Bezier"
+			#elif ptype == "Arc":
+			#	print "Arc"
 			elif ptype == "Line":
-				print "Line"
+				#print "Line"
+				#print "el.start = (%f,%f), el.end = (%f,%f)" % (el.start.real, el.start.imag, el.end.real, el.end.imag)
+				#points = []
+				if el.start == el.end:
+					points = [el.start]
+				else:
+					points = bezier_points(el, a, eps)
+				#points = line_points(el, a)
+				for p in points:
+					print "%(x)f %(y)f" % {"x":p.real+mazak.xoffset, "y":p.imag+mazak.yoffset}
+				#P = points[-1]
+			P = points[-1]
+			#P = complex(points[-1].real+mazak.xoffset, points[-1].imag+mazak.yoffset)
+			#mazak.xoffset += P.real - el.end.real
+			#mazak.yoffset += P.imag - el.end.imag
+				#print "Line"
 			print "PODNIES"
 	print "KONIEC"
+	
 	#p0 = complex(491.87909, 742.65982)
 	#p1 = complex(510.69338, 724.2192)
 	#p2 = complex(517.39227, 706.1286)
---
+++ odleglosci.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python2
+from math import sqrt
+
+def distance(a, b):
+	return sqrt((a.real - b.real)**2 + (a.imag - b.imag)**2)
+
+f = open('punkty.txt', 'r')
+line = f.readline()
+
+P = complex(0,0)
+
+while line:
+	if line.find('START') != -1:
+		print line,
+	elif line.find('RYSUJ') != -1:
+		print line,
+	elif line.find('PODNIES') != -1:
+		print line,
+	elif line.find('KONIEC') != -1:
+		print line,
+	elif line.find('=') == -1:
+		x = float(line[:line.find(' ')])
+		y = float(line[line.find(' ')+1:-1])
+		Pnew = complex(x, y)
+		d = distance(P, Pnew)
+		print "%f" % d
+		P = Pnew
+	line = f.readline()
