cefbe8f5e10484c2319bfbba9e480eb733f0ab93
hun7err <hun7err@gmail.com>
2013-04-10 12:28:18 +0200

    added comments + images

--- generator.py
+++ generator.py
@@ -5,23 +5,22 @@ from mazakodron import *
 from svg.path import *
 from sys import argv
 
-def dist2(curve, p, t):
+def dist2(curve, p, t):	# odleglosc podniesiona do kwadratu
 	return (p.real - curve.point(t).real)**2 + (p.imag - curve.point(t).imag)**2
 
-def func(curve, p, t, a):
+def func(curve, p, t, a):	# funkcja d^2 - a^2
 	return dist2(curve, p, t) - a**2
 
-def distance(a, b):
+def distance(a, b):	# odleglosc miedzy punktami a i b
 	return sqrt((a.real - b.real)**2 + (a.imag - b.imag)**2)
 
-def bezier_points(curve, a, eps):
-	p = curve.start
-	t = 0
-	left = 0
-	right = 1
-	points = []
-	#points.append(curve.start)
-	while t < 1:
+def bezier_points(curve, a, eps):	# krzywa beziera o danym t, przy dokladnosci a i stalej zaokraglen eps
+	p = curve.start	# zaczynamy od poczatku krzywej
+	t = 0		# wowczas t = 0
+	left = 0	# lewa = 0
+	right = 1	# prawa = 0
+	points = []	# punkty do zwrocenia
+	while t < 1:	# tu zwykla metoda polowienia, kazdy powinien miec na informatyce w liceum. Nie bede sie rozpisywal
 		left = t
 		right = 1
 		while(dist2(curve, p, right) < a**2):
@@ -36,18 +35,19 @@ def bezier_points(curve, a, eps):
 		p = curve.point(t)
 		points.append(p)
 	lastt = curve.point(1)
-	d = distance(p, lastt)
-	if lastt.imag - p.imag == 0:
+	d = distance(p, lastt)		# tu sie zaczyna magia. Jako ze mozemy pozostawic na koncu odcinek o dlugosci (dlugosc_luku mod a), to musimy dorysowac dodatkowy odcinek ktory bedzie szedl przez ostatni wyliczony punkt i punkt konczacy krzywa oraz mial dlugosc 'a'. W tym celu korzystamy z twierdzenia Talesa i podobienstwa trojkatow
+	if lastt.imag - p.imag == 0:	# przypadki szczegolne - punkty wspolliniowe
 		AB = a
-	else:
-		AB = float(a*abs(lastt.real-p.real))/float(d)
-	if lastt.real - p.real == 0:
+	else:				# tu juz normalny przypadek
+		AB = float(a*abs(lastt.real-p.real))/float(d)	# z twierdzenia Talesa
+	if lastt.real - p.real == 0:	# kolejny wspomniany przypadek szczegolny
 		BC = a
-	else:
-		BC = float(AB*abs(lastt.imag - p.imag)) / float(abs(p.real - lastt.real))
+	else:				# ...i znow normalny
+		BC = float(AB*abs(lastt.imag - p.imag)) / float(abs(p.real - lastt.real))	# z podobienstwa trojkatow
 	lastx = 0
 	lasty = 0
 
+	# sgn to znak, ktory okresla po ktorej stronie ma sie znalezc punkt
 	if(p.real > lastt.real):
 		sgn = 1
 	else:
@@ -63,196 +63,69 @@ def bezier_points(curve, a, eps):
 
 	return points
 
-def line_points(line, a):
-	d = distance(line.start, line.end)
-	x_diff = 0
-	y_diff = 0
-
-	# naprawic odleglosci tutaj bo wychodza jakies popieprzone, np. 18
-
-	if d < a:
-		if line.end.imag - line.start.imag == 0:
-			x_diff = a
-		else:
-			x_diff = float(a*abs(line.end.real - line.start.real))/float(d)
-		if line.start.real - line.end.real == 0:
-			y_diff = a
-		else:
-			y_diff = float(x_diff * abs(line.end.imag - line.start.imag)) / float(abs(line.start.real - line.end.real))
-		d = a
-	else:
-		x_diff = abs(line.start.real - line.end.real)
-		y_diff = abs(line.start.imag - line.end.imag)
-		# oblicz z talesa punkt tak, aby przychodzil przez line.start i line.end ale lezal w odleglosci a
-		# w innym wypadku licz normalnie x_diff, y_diff
-		#
-		# rozwazyc przypadek, gdy d < a ale punkty leza w linii prostej
-	n = int(floor(float(d)/float(a)))
-	#x_diff = abs(line.start.real - line.end.real)
-	#y_diff = abs(line.start.imag - line.end.imag)
-	
-	#if n != 0:
-	x_off = float(x_diff)/float(n)
-	y_off = float(y_diff)/float(n)
-	#else:
-	#	x_off = y_off = 0
-
-	# czy to aby na pewno jest dobrze?
-	# czy zapewnione jest przejscie o krok a?
-	# lol, nope
-
-	x = line.start.real
-	y = line.start.imag
-
-	points = []
-	sgn_x = 0
-	sgn_y = 0
-	if (line.start.real < line.end.real):
-		sgn_x = 1
-	else:
-		sgn_x = -1
-	if (line.start.imag < line.end.imag):
-		sgn_y = 1
-	else:
-		sgn_y = -1
-	
-	for i in range(1,n+2):
-		points.append(complex(x,y))
-		x += sgn_x * x_off
-		y += sgn_y * y_off
-	#print "a = %(a)d, dist = %(d)f" % {"a" : a, "d":distance(points[0], points[-1])}
-	return points
-
 if __name__ == "__main__":
-	'''
-	to-do:
-	dodawac ostatni punkt na krzywej: skorzystac z metody
-	podobnej do aproksymacji punktow na prostej. Dlaczego?
-	Dlatego, ze bez tego czasem generuje same punkty poczatkowe
-	krzywych. Ew. zrobic tez aproksymacje punktow na elipsie,
-	ale to zalezy od tego czy dos w swoim svg podaje elipsy/luki.
-	Co do szukania ostatnich punktow - wystarczy poprowadzic prosta
-	przez ostatni znaleziony punkt i punkt koncowy krzywej ktory jest
-	polozony w odleglosci rownej zadanej dokladnosci od ostatniego
-	punktu. Mozna ew. skorzystac (chyba) z tw. Talesa do znalezienia
-	tego.
-	Oprocz tego dodac zamiane x/y w print na koncu w zaleznosci
-	od layoutu: moze byc jakas funkcja printPoints(tab, layout)
-	gdzie layout byloby horizontal/vertical
-	Dodac tez przesuwanie wzgledem ostatnio znalezionego punktu
-	i prowadzenie prostych od ostatnio znalezionego punktu do nastepnego
-	aby dystans dzielil sie na rowne odcinki dlugosci a gdzie a jest
-	dokladnoscia
-
-	uklad wspolrzednych:
-	+------> x
-	|
-	|
-	|
-	v y
-
-	'''
-
-	# <TEST>
-	'''
-	p0 = complex(112.3125, 913.29968)
-	p1 = complex(512.3125, 912.17468)
-
-	tab = line_points(Line(p0,p1), 25)
-	for el in tab:
-		print "%(x)f %(y)f" % {"x":el.real, "y":el.imag}
-	'''
-	# </TEST>
-	
 	filename = ''
 	try:
 		filename = argv[1]
 	except IndexError:
-		raise AssertionError('Brak nazwy pliku')
-	a = 10
-	eps = 0.001
+		raise AssertionError('Brak nazwy pliku')	# chronimy sie przed zapominalstwem uzytkownikow ;)
+	a = 10		# dokladnosc rysowania w milimetrach
+	eps = 0.001	# stala zaokraglen numerycznych
 
 	mazak = Mazakodron()
-	mazak.load(filename)
-	#print mazak.getDocDims()
-	mazak.loadPaths()
-	print "START"
+	mazak.load(filename)	# ladujemy plik SVG
+	mazak.loadPaths()	# ladujemy sciezki
+	print "START"		# start rysowania
 	
 	P = complex(0,0)
 
-	for path in mazak.getPaths():
+	for path in mazak.getPaths():	# przerabianie sciezek na punkty
 		for el in path:
-			#print "P = %f %f" % (P.real, P.imag)
-			#print "el = %f %f" % (el.start.real, el.start.imag)
+			# linia od ostatniego punktu do poczatku sciezki - el jest sciezka
 			if P == complex(0,0):
 				line = Line(P, complex(el.start.real+mazak.xoffset,el.start.imag+mazak.yoffset))
 			else:
 				line = Line(P, el.start)
 			if line.start != line.end:
-				#linepoints = line_points(line, a)
 				linepoints = bezier_points(line, a, eps)
 				for point in linepoints:
-					#if point != line.start:
 					print "%(x)f %(y)f" % {"x": point.real, "y":point.imag}
 				last = complex(0,0)
 				if P == complex(0,0):
 					last = complex(linepoints[-1].real-mazak.xoffset,linepoints[-1].imag-mazak.yoffset)
 				else:
 					last = linepoints[-1]
+				# wyrownujemy sciezki tak, aby przesuniecie wynikajace z zaokraglenia trasy przejazdu do wielokrotnosci dokladnosci nie powodowaly bledow przesuniec
 				mazak.xoffset += last.real - el.start.real
 				mazak.yoffset += last.imag - el.start.imag
-				
-				# tutaj dodac offsety na x i y (mazak.xoffset, mazak.yoffset)
-				# i znak w zaleznosci od tego czy pierwotne > nowe czy nie
-
+			# opusc mazak
 			print "OPUSC"
+			
+			# sprawdz rodzaj sciezki
 			ptype = str(type(el))
 			ptype = ptype[ptype.rfind('.')+1:ptype.rfind("'")]
 			points = []
 			
+			# wypisz punkt
 			if ptype == "CubicBezier":
-				#print "Cubic Bezier"
 				points = bezier_points(el, a, eps)
 				for p in points:
 					print "%(x)f %(y)f" % {"x":p.real+mazak.xoffset, "y":p.imag+mazak.yoffset}
-				#P = points[-1]
 			elif ptype == "QuadraticBezier":
-				#print "Quadratic Bezier"
 				points = bezier_points(el, a, eps)
 				for p in points:
 					print "%(x)f %(y)f" % {"x":p.real+mazak.xoffset, "y":p.imag+mazak.yoffset}
-				#P = points[-1]
-				#print "Quadratic Bezier"
 			#elif ptype == "Arc":
 			#	print "Arc"
 			elif ptype == "Line":
-				#print "Line"
-				#print "el.start = (%f,%f), el.end = (%f,%f)" % (el.start.real, el.start.imag, el.end.real, el.end.imag)
-				#points = []
 				if el.start == el.end:
 					points = [el.start]
 				else:
 					points = bezier_points(el, a, eps)
-				#points = line_points(el, a)
 				for p in points:
 					print "%(x)f %(y)f" % {"x":p.real+mazak.xoffset, "y":p.imag+mazak.yoffset}
-				#P = points[-1]
+			# ostatni punkt sciezki staje sie punktem z ktorego przejezdzamy do kolejnego
 			P = points[-1]
-			#P = complex(points[-1].real+mazak.xoffset, points[-1].imag+mazak.yoffset)
-			#mazak.xoffset += P.real - el.end.real
-			#mazak.yoffset += P.imag - el.end.imag
-				#print "Line"
+			# podnies mazak
 			print "PODNIES"
 	print "KONIEC"
-	
-	#p0 = complex(491.87909, 742.65982)
-	#p1 = complex(510.69338, 724.2192)
-	#p2 = complex(517.39227, 706.1286)
-	#p3 = complex(513.45907, 684.69745)
-
-	#c = CubicBezier(p0,p1,p2,p3)
-	#p = bezier_points(c, 5, 0.001)
-	#for el in p:
-	#	print str(el)
-	
-	#print "Hello, world!"
--- mazakodron.py
+++ mazakodron.py
@@ -1,20 +1,21 @@
-from svg.path import *
-import xml.etree.ElementTree as et
+from svg.path import *			# do przerabiania sciezek
+import xml.etree.ElementTree as et	# do ladowania XMLa z pliku SVG
 
 class Mazakodron:
-	scaleFactor = float(1.0)
-	sheetWidth = 0
-	sheetHeight = 0
-	docWidth = 0
-	docHeight = 0
-	tree = None
-	paths = []
-	xoffset = 0
-	yoffset = 0
+	scaleFactor = float(1.0)	# wspolczynnik skalowania
+	sheetWidth = 0			# szerokosc kartki
+	sheetHeight = 0			# wysokosc kartki
+	docWidth = 0			# szerokosc dokumentu
+	docHeight = 0			# wysokosc dokumentu
+	tree = None			# drzewo XML
+	paths = []			# sciezki z pliku SVG
+	xoffset = 0			# przesuniecie na osi X
+	yoffset = 0			# przesuniecie na osi Y
 
-	def __init__(self, paperFormat = 'A4', filename=None):
+	def __init__(self, paperFormat = 'A4', filename=None):	# konstruktor klasy: domyslnie ustawiamy format kartki na A4
 		if(filename != None):
 			self.load(filename)
+		# dalej sa wymiary w milimetrach kartek o danym formacie
 		if paperFormat == 'A5':
 			self.sheetWidth = 148
 			self.sheetHeight = 210
@@ -28,55 +29,45 @@ class Mazakodron:
 			self.sheetWidth = 420
 			self.sheetHeight = 594
 
-	def load(self, svgFile):
+	def load(self, svgFile):	# zaladuj plik o nazwie svgFile
 		try:
-			self.tree = et.parse(svgFile)
-			"""
-			elements = []
-			for el in tree.getroot().findall('.//'):
-				if el.tag.find('path') != -1:
-					elements.append(el)
-			paths = list(parse_path(el.get('d')) for el in elements)
-			"""
-			self.docWidth = int(self.tree.getroot().get('width'))
-			self.docHeight = int(self.tree.getroot().get('height'))
-			if self.docWidth > self.docHeight:
+			self.tree = et.parse(svgFile)					# wczytaj elementy XML
+			self.docWidth = int(self.tree.getroot().get('width'))		# pobierz szerokosc dokumentu
+			self.docHeight = int(self.tree.getroot().get('height'))		# pobierz wysokosc dokumentu
+			if self.docWidth > self.docHeight:	# "obroc kartke" aby dopasowac do dokumentu
 				self.sheetWidth, self.sheetHeight = self.sheetHeight, self.sheetWidth
-			'''print float(self.sheetWidth)/float(self.docWidth)
-			print float(self.sheetHeight)/float(self.docHeight)'''
-			self.scaleFactor = min(float(self.sheetWidth)/float(self.docWidth), float(self.sheetHeight)/float(self.docHeight))
-			self.docWidth *= self.scaleFactor
-			self.docHeight *= self.scaleFactor
-			self.xoffset = abs(self.docWidth-self.sheetWidth)/2
-			self.yoffset = abs(self.docHeight - self.sheetHeight)/2
-			if self.docWidth > self.docHeight:
+			
+			self.scaleFactor = min(float(self.sheetWidth)/float(self.docWidth), float(self.sheetHeight)/float(self.docHeight))	# oblicz wspolczynnik skalowania
+			self.docWidth *= self.scaleFactor			# przeskaluj dokument
+			self.docHeight *= self.scaleFactor			# jak wyzej
+			self.xoffset = abs(self.docWidth-self.sheetWidth)/2	# wycentruj
+			self.yoffset = abs(self.docHeight - self.sheetHeight)/2	# jak wyzej
+			if self.docWidth > self.docHeight:	# obroc przesuniecia aby dostosowac do kartki i dokumentu
 				self.xoffset,self.yoffset = self.yoffset,self.xoffset
 		except (AttributeError, TypeError):
 			raise AssertionError('No chyba nie. svgFile powinno byc ciagiem znakow')
-	def loadPaths(self):
-		elements = []
-		for el in self.tree.getroot().findall('.//'):
-			if el.tag.find('path') != -1:
-				elements.append(el)
-		self.paths = list(parse_path(el.get('d')) for el in elements)
-		''' przegladac sciezki i dla kazdego punktu *= scaleFactor '''
-		for path in self.paths: # skalujemy obraz do wielkosci arkusza po ktorym bedziemy rysowac
+	def loadPaths(self):	# zaladuj sciezki
+		elements = []	# elementy XML
+		for el in self.tree.getroot().findall('.//'):	# pobierz wszystkie elementy
+			if el.tag.find('path') != -1:		# jesli element jest sciezka
+				elements.append(el)		# to wrzuc go do listy
+		self.paths = list(parse_path(el.get('d')) for el in elements)	# utworz liste zawierajaca sciezki w rozumieniu svg.path
+		
+		for path in self.paths: # skalujemy punkty do rozmiarow arkusza po ktorym bedziemy rysowac
 			for el in path:
-				ptype = str(type(el))
-				ptype = ptype[ptype.rfind('.')+1:ptype.rfind("'")]
-				el.start *= self.scaleFactor
-				el.end *= self.scaleFactor
+				ptype = str(type(el))	# pobierz typ ptype
+				ptype = ptype[ptype.rfind('.')+1:ptype.rfind("'")]	# wyluskuje nazwe, np. CubicBezier
+				el.start *= self.scaleFactor	# przeskaluj
+				el.end *= self.scaleFactor	# jak wyzej
 				if ptype == "CubicBezier":
-					#el.start *= scaleFactor
-					el.control1 *= self.scaleFactor
-					el.control2 *= self.scaleFactor
-					#el.end *= scaleFactor
+					el.control1 *= self.scaleFactor	# tez skalowanie
+					el.control2 *= self.scaleFactor	# ...i znowu...
 				elif ptype == "QuadraticBezier":
-					el.control *= self.scaleFactor
+					el.control *= self.scaleFactor	# czy to sie kiedys skonczy?
 				elif ptype == "Arc":
-					el.radius *= scaleFactor
-				#print ptype
-	def getPaths(self):
+					el.radius *= scaleFactor	# tak, to ostatni!
+				
+	def getPaths(self):	# zwroc liste sciezek
 		return self.paths
-	def getDocDims(self):
+	def getDocDims(self):	# pobierz rozmiar dokumentu
 		return complex(self.docHeight, self.docWidth)
